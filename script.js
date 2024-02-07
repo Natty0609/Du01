7d4a1ab72c35c82c12b8b3261d6583ce7014f38f
Nataliya Tahova <ntahova@gmail.com>
2024-02-07 20:09:46 +0100

    domaci ukol

---
+++ script.js
@@ -0,0 +1,5 @@
+//Fahrnheit vs. Celsius
+let farenheit = prompt("Zadej ° ve Farenheit:");
+
+let vysledek = celsius = 5 / 9 * (farenheit - 32);
+document.body.innerHTML = "Teplota ve Celsius je: " + vysledek;
